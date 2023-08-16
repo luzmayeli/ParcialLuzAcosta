869b4fca08f66239a6b27c0d81e0009fd8ce1c44
luzmayeli <mayelisacosta@gmail.com>
2023-08-16 18:25:43 -0500

    termino el parcial

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,10 @@ import './App.css'
 import { Card } from './Components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-
       <Card/>
     </>
   )
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,9 +1,66 @@
 import React from 'react'
+import { useState } from 'react'
+import './Card.css'
 
 export const Card = () => {
-  return (
-    <div>Hola, soy una tarjeta
-        <h1>Esta es una prueba segunda de commit </h1>
-    </div>
-  )
+
+
+    const [cancion, setCancion] = useState({
+      name: '',
+      artist: '',
+      cliente: ''
+      
+  })
+
+
+
+  const [show, setShow] = useState(false)
+  const [error, setError] = useState(false)
+
+  const handleSubmit = (e) => {
+      e.preventDefault()
+      if(cancion.name.length > 2 && cancion.artist.length > 5){
+          setShow(true)
+          setError(false)
+      } else {
+          setError(true)
+          setShow(false)
+      }
+  }
+
+  console.log(cancion)
+return (
+  <form className="formulario" onSubmit={handleSubmit}>
+      <div>
+        <label >Nombre de la Canción: </label> <br/>
+        <input type="text" onChange={(e) => setCancion({...cancion, name: e.target.value})}/>
+      </div>
+     <div>
+        <label >Cantante o Agrupación: </label> <br/>
+        <input type="text" onBlur={(e) => setCancion({...cancion, artist: e.target.value})}/>
+     </div>
+     <div>
+        <label >Tu nombre es: </label> <br/>
+        <input type="text" onBlur={(e) => setCancion({...cancion, cliente: e.target.value})}/>
+     </div>
+
+    <button>Enviar</button>
+
+      {error && <h6 className='error' >Por favor chequea que la información sea correcta</h6>}
+
+      {show ? <>
+          
+          <p className='ook'>
+            <h3>Hola {cancion.cliente}!</h3>
+            en minutos reproduciremos tu cancion: <br/> {cancion.name}</p>
+          
+          
+      </>
+      : null
+      }
+
+  </form>
+)
+
+
 }
